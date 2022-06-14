2bd845eeb4f57140095c04a92de535d59861336a
gmanatov <jfhipr@gmail.com>
2022-06-14 17:19:54 -0500

    Final upload

---
+++ wargame/ContentView.swift
@@ -0,0 +1,113 @@
+//
+//  ContentView.swift
+//  wargame
+//
+//  Created by first on 5/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    @State var playerCard = "card2"
+    @State var cpuCard = "card2"
+    
+    /*
+    var playerScore = 1
+    var cpuScore = 1
+    var playerCard = "card2"
+    var cpuCard = "card2"
+    */
+     
+    var body: some View {
+        
+        ZStack{
+            
+            Image("background").ignoresSafeArea()
+            
+            VStack{
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                    var playerRand = Int.random(in: 2...14)
+                    var cpuRand = Int.random(in: 2...14)
+                    
+                    //Updating cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    //Updating score
+                    if playerRand > cpuRand {
+                        playerScore += 1}
+                    else if playerRand == cpuRand{}
+                    else {cpuScore += 1}
+                    
+                    
+                }, label: {Image("dealbutton")})
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                        Text("")
+                        Text(String(playerScore))
+                    }
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                        Text("")
+                        Text(String(cpuScore))
+                    }
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    playerCard = "card2"
+                    cpuCard = "card2"
+                    playerScore = 0
+                    cpuScore = 0
+                }, label: {Text("RESET")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.red)})
+                
+                Spacer()
+                
+            }
+            
+            
+        }
+        
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wargame/wargameApp.swift
@@ -0,0 +1,17 @@
+//
+//  wargameApp.swift
+//  wargame
+//
+//  Created by first on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct wargameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
